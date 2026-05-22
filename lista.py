155ef956f6bc9fa99e0518f6c899524e3dcc46b3
marcelorafaelfaro-sketch <marcelorafaelfaro@gmail.com>
2026-05-22 19:48:02 -0300

    Update lista.py

--- lista.py
+++ lista.py
@@ -1,3 +1,8 @@
-list = [1, "uva",2,3,"caroço"]
-for i in list:
+lista = [1, "uva",2,3,"caroço","almondega",332]
+for i in lista:
     print(i)
+alguma_coisa = [1.4,4.4,"queijo"]
+for a in alguma_coisa:
+    print(a)
+for i in range(len(lista)):
+    print (lista)
