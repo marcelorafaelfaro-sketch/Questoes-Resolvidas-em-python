2b74880805debfe8a439551fd2b408366a738cc9
marcelorafaelfaro-sketch <marcelorafaelfaro@gmail.com>
2026-06-23 19:27:08 -0300

    Ta aqui a questao 16 do fabio santana

--- Questao_20_Fabio_Santana.py
+++ Questao_20_Fabio_Santana.py
@@ -1,5 +1,6 @@
-#20)Escreva um programa que exiba a soma dos números múltiplos de 7 no intervalo
-#[100, 200].
+'''20)Escreva um programa que exiba a soma dos números múltiplos de 7 no intervalo
+[100, 200].
+'''
 numero = int(input("Informe o numero: "))
 operacao = 0
 for multiplo in range(1,201):
