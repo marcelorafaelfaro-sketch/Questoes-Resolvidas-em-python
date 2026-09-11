e757f5219d99313e9b3fcc2542b2551a1c083da1
marcelorafaelfaro-sketch <marcelorafaelfaro@gmail.com>
2026-09-11 16:10:09 -0300

    fiz pequenos testes de lista e estou utilizando alguns recursos do modulo math

--- lista.py
+++ lista.py
@@ -1,8 +1,11 @@
-lista = [1, "uva",2,3,"caroço","almondega",332]
-for i in lista:
-    print(i)
+# lista = [1, "uva",2,3,"caroço","almondega",332]
+# for i in lista:
+#     print(i)
 alguma_coisa = [1.4,4.4,"queijo"]
-for a in alguma_coisa:
-    print(a)
-for i in range(len(lista)):
-    print (lista)
+# for a in alguma_coisa:
+#     print(a)
+# for i in range(len(lista)):
+#     print (lista)
+
+
+print(type(len(alguma_coisa)))
---
+++ matematica.py
@@ -0,0 +1,5 @@
+import math
+n1 = float(input("Informe o numero: "))
+raiz_quadrada = math.sqrt(n1)
+print(math.ceil(raiz_quadrada))
+print(round(raiz_quadrada,4))
