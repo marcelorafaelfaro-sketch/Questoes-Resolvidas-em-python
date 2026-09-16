8fa899f55c8c3fbea4f5059f630186a859240f0d
marcelorafaelfaro-sketch <marcelorafaelfaro@gmail.com>
2026-09-15 21:19:26 -0300

    Teste do modulo datetime e etc

--- date_time.py
+++ date_time.py
@@ -1,1 +1,15 @@
 import datetime
+print(datetime.MINYEAR)
+print(datetime.MAXYEAR)
+minha_data = datetime.datetime(2007, 10, 17)
+silvia_data = datetime.datetime(2008,1,19)
+print(minha_data.year)
+print(minha_data.month)
+print(minha_data.day)
+dia_de_hoje = (datetime.date.today())
+print(dia_de_hoje)
+hoje = datetime.datetime.now()
+idade_marcelos = hoje - minha_data
+idade_silvia = hoje - silvia_data
+print(idade_marcelos)
+print(idade_silvia)
