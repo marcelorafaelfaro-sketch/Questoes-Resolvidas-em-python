4e2d6f98d2ca81f123052175ce423cc830498361
marcelorafaelfaro-sketch <marcelorafaelfaro@gmail.com>
2026-09-17 19:32:00 -0300

    Teste de dicionario e afins

---
+++ Teste_dicionarios.py
@@ -0,0 +1,9 @@
+dicionario1 = {
+    "Marcelo": 3,
+    "Aparencia": "alto","lindo":"charmoso",
+
+}
+print(type(dicionario1))
+print(dicionario1)
+print(f"Aqui está seu {dicionario1["Aparencia"]}")
+print(f"O tamanho disso é {len(dicionario1)}")
