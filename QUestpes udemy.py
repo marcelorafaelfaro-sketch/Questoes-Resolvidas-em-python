8c84f8693fbb727af4affc6aa19f0616e41a2a39
Marcelo <marcelo.faro@discente.ufra.edu.br>
2026-09-25 18:00:16 -0300

    Fiz questoes da udemy no dia 25 09 2026 e foi isso

---
+++ QUestpes udemy.py
@@ -0,0 +1,42 @@
+"""
+1. Faça um programa que determine e mostre os cinco primeiros múltiplos de 3, considerando números
+maiores que 0.
+2. Faça um programa que utilize o comando while para mostrar na tela uma contagem regressiva, iniciando
+em 10 e terminando em 0. Mostre também uma mensagem “FIM!” após a contagem.
+3. Faça um programa que declare um inteiro, inicialize-o com 0, incremente-o de 1000 em 1000, imprimindo
+seu valor na tela, até que seu valor seja 100000 (cem mil).
+"""
+n1 = int(input("Informe o numero e eu irei dizer seus 10 primeiros multiplos de 3: "))
+contador = int = 0
+for i in range(1, 10):
+    contador = i * 3
+    print(contador)
+print("""
+
+
+
+
+
+
+
+
+
+
+
+""")
+limite = -1
+for a in range(10, limite,-1):
+    print(f"{a}")
+print("FIM")
+print("""
+
+
+
+
+
+
+
+
+""")
+for numero in range(0,100001, 1000 ):
+    print(numero)
